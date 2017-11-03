c9d44e68db00457a11e190a1283bad46b7948ac9
Christopher M. Judd <javajudd@gmail.com>
2017-11-03 17:32:47 -0400

    Created project using http://start.spring.io/ & Web, Security, JPA.

---
+++ src/main/java/net/javajudd/attis/AttisApplication.java
@@ -0,0 +1,12 @@
+package net.javajudd.attis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttisApplication.class, args);
+	}
+}
