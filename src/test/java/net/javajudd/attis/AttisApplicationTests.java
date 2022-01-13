04b2fb28fe3fbecd8d22a66fe85391ed681ffb9f
Christopher M. Judd <javajudd@gmail.com>
2022-01-13 16:48:19 -0500

    Generated new app from https://start.spring.io with dependencies web, jpa and spring security.

---
+++ src/test/java/net/javajudd/attis/AttisApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javajudd.attis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
