83fbd1f1d5fcf37fbf06c8dd3f773849a12cd7d8
Christopher M. Judd <javajudd@gmail.com>
2022-08-24 17:25:05 -0400

    Added policies and environment variables. This got the cdk dot deploy and work.

--- cdk/lib/attis-cdk-stack.ts
+++ cdk/lib/attis-cdk-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
@@ -27,6 +28,11 @@ export class AttisCdkStack extends Stack {
       taskImageOptions: {
         image: ecs.ContainerImage.fromRegistry("public.ecr.aws/g4v9z1z2/attis:latest"),
         containerPort:8080,
+        environment: {
+          'MESSAGE_SEND_FROM':'xxx',
+          'SPRING_MAIL_PASSWORD':'xxx',
+          'SPRING_MAIL_USERNAME':'xxx'
+        }
       },
       securityGroups: [securityGroup],
       assignPublicIp: true,
@@ -35,6 +41,22 @@ export class AttisCdkStack extends Stack {
       openListener: true,
       loadBalancerName: 'attis-lb',
     });
+
+    loadBalancedFargateService.taskDefinition.addToTaskRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        'iam:CreateUser',
+        'iam:TagUser',
+        'iam:GetUser',
+        'iam:CreateAccessKey',
+        'iam:CreateLoginProfile',
+        'iam:AddUserToGroup',
+        'iam:ListAccountAliases',
+        'ec2:RunInstances',
+        'ec2:CreateTags'
+      ],
+      resources: ['*']
+    }));
 
     loadBalancedFargateService.targetGroup.configureHealthCheck({
       path: '/images/attis.png',
