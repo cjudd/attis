ab81f57a4daa332abb5fdc5ec9d4afc7c4272bd2
Christopher M. Judd <javajudd@gmail.com>
2022-08-26 13:55:54 -0400

    Moved email credentials and secrets to secret manager.

--- cdk/lib/attis-cdk-stack.ts
+++ cdk/lib/attis-cdk-stack.ts
@@ -4,6 +4,7 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as sm from 'aws-cdk-lib/aws-secretsmanager';
 
 export class AttisCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -20,6 +21,9 @@ export class AttisCdkStack extends Stack {
 
     const securityGroup = ec2.SecurityGroup.fromLookupByName(this, 'dev-sg', 'devvm-default-sg', vpc);
 
+    const secrets = sm.Secret.fromSecretCompleteArn(this, "attis-secrets",
+        "arn:aws:secretsmanager:" + this.region +":" + this.account + ":secret:attis-JN36pR");
+
     const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'attis-service', {
       cluster,
       memoryLimitMiB: 2048,
@@ -28,10 +32,10 @@ export class AttisCdkStack extends Stack {
       taskImageOptions: {
         image: ecs.ContainerImage.fromRegistry("public.ecr.aws/g4v9z1z2/attis:latest"),
         containerPort:8080,
-        environment: {
-          'MESSAGE_SEND_FROM':'xxx',
-          'SPRING_MAIL_PASSWORD':'xxx',
-          'SPRING_MAIL_USERNAME':'xxx'
+        secrets: {
+          'SPRING_MAIL_USERNAME': ecs.Secret.fromSecretsManager( secrets,'spring.mail.username'),
+          'SPRING_MAIL_PASSWORD': ecs.Secret.fromSecretsManager( secrets, 'spring.mail.password'),
+          'MESSAGE_SEND_FROM':ecs.Secret.fromSecretsManager( secrets, 'message.send.from')
         }
       },
       securityGroups: [securityGroup],
@@ -53,7 +57,8 @@ export class AttisCdkStack extends Stack {
         'iam:AddUserToGroup',
         'iam:ListAccountAliases',
         'ec2:RunInstances',
-        'ec2:CreateTags'
+        'ec2:CreateTags',
+        'secretsmanager:GetSecretValue'
       ],
       resources: ['*']
     }));
